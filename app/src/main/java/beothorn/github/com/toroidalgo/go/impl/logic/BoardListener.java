6d9ffc7ad0d3f3b7be3c4252c2851b6db90f101a
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-03-02 04:07:31 -0300

    Minor

--- app/src/main/java/beothorn/github/com/toroidalgo/go/impl/logic/BoardListener.java
+++ app/src/main/java/beothorn/github/com/toroidalgo/go/impl/logic/BoardListener.java
@@ -2,7 +2,7 @@ package beothorn.github.com.toroidalgo.go.impl.logic;
 
 public interface BoardListener {
 
-	void updateScore(int _blackScore, float _whiteScore);
+	void updateScore(int _blackScore, float _whiteScore); //White can have 6.5 Komi, for example.
 	void nextToPlay(StoneColor _nextToPlay);
 	void nextToPlayOnPass(StoneColor _nextToPlay);
 
